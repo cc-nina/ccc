e7dfb4ef88d72a7752b8072e8dfdf7c65e25c4b8
cc-nina <134860155+cc-nina@users.noreply.github.com>
2024-08-19 21:05:20 +0000

    bg [#311847]

--- my-app/src/components/Header.svelte
+++ my-app/src/components/Header.svelte
@@ -12,7 +12,7 @@
 <header
     class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
         (y > 0
-            ? " py-4 bg-slate-950 border-violet-950"
+            ? " py-4 bg-[#311847] border-violet-950"
             : " py-6 bg-transparent border-transparent")}
 >
     <h1 class="font-medium">
