a24bfa3a3f29834083797b50f6cd9f51c069fbb1
cc-nina <134860155+cc-nina@users.noreply.github.com>
2024-08-21 21:42:17 +0000

    placeholder

--- my-app/src/components/Footer.svelte
+++ my-app/src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer id="contact"
-    class="py-20 sm:py-32 bg-[#311847] border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
 >
     <div
             class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
